05e55c6c3940f3b645dbcdc99a4ff05af766be96
Krishna <maheshwarikrishna67@gmail.com>
2025-09-25 11:27:37 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Krishna Maheshwari',
+    author_email='maheshwarikrishna67@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
